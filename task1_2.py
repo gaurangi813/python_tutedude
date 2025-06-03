237373dd27f770f3840d33f8691ca271ab88b6e2
Gaurangi Tripathi <115712833+gaurangi813@users.noreply.github.com>
2025-06-03 19:27:27 +0530

    Add files via upload

---
+++ task1_2.py
@@ -0,0 +1,5 @@
+a = int(input("Enter an integer: "))
+if a % 2 == 0:
+    print(f"{a} is even.")
+else:
+    print(f"{a} is odd.")
---
+++ task2_2.py
@@ -0,0 +1,4 @@
+a = 0
+for n in range(1, 51):
+    a += n 
+print("The sum of integers from 1 to 50 is:", a)
